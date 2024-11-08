8b012361fcb6a62423c6ad4b24411d83b5dcfc93
Frank Wang <broodstar_frank@hotmail.com>
2024-11-08 16:38:20 -0600

    improve training parameters

--- src/train.py
+++ src/train.py
@@ -13,16 +13,16 @@ if __name__ == "__main__":
     experimentName = "place_cells"
 
     # model = ResNet(game, 9, 128, getTorchDevice())
-    model = PlaceCellResNet(game, 9, 128, 256, 5376, 100, 0.0001, getTorchDevice())
+    model = PlaceCellResNet(game, 9, 128, 256, 5376, 100, 0.01, getTorchDevice())
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
 
     args = {
         "C": 2,
         "num_searches": 600,
-        "num_iterations": 15,
+        "num_iterations": 8,
         "num_selfPlay_iterations": 500,
         "num_parallel_games": 100,
-        "num_epochs": 4,
+        "num_epochs": 16,
         "batch_size": 128,
         "temperature": 1.25,
         "dirichlet_epsilon": 0.25,
